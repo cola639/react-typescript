4a313e9c022774d19d93b95fafcc35a261ed1f64
cola639 <337507950@qq.com>
2022-09-01 21:17:00 +0800

    fix: store module

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,9 @@
+export type Dimension = number | string
+
+export type xData = number[] | string[]
+
+export type LanguageType = 'de' | 'en'
+
+export type ThemeType = 'light' | 'dark'
+
+export type Severity = 'success' | 'error' | 'info' | 'warning'
