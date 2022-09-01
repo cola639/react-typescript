339e2f0c98b59659f177406f1cad8da121e0e1e0
cola639 <337507950@qq.com>
2022-09-01 21:22:25 +0800

    Revert "fix: store module"
    This reverts commit 4a313e9c022774d19d93b95fafcc35a261ed1f64.

--- src/interfaces/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export type Dimension = number | string
-
-export type xData = number[] | string[]
-
-export type LanguageType = 'de' | 'en'
-
-export type ThemeType = 'light' | 'dark'
-
-export type Severity = 'success' | 'error' | 'info' | 'warning'
