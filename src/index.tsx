60a7b425e11a3b3166dbe0553ae0a1817f59e519
cola639 <337507950@qq.com>
2022-09-05 02:01:16 +0800

    add: SCSS && ENV config

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals' // web report
+import './style/global.scss'
 
 ReactDOM.render(
     <React.StrictMode>
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+// import styles from './index.module.scss'
+import './index.scss' // difference from xx.modile.css
+
 const OnlineVote: React.FC = () => {
     return (
-        <div>
-            <span>OnlineVote</span>
-        </div>
+        // <div className={`flex-center ${styles.online}`}>
+        //     <div className={styles.online_wrap}></div>
+        //     <span>OnlineVote</span>
+        // </div>
+        <>
+            <div className="home">
+                <div className="home_top">
+                    {/* <head-title title="网上投票" url="/person" />  */}
+                    <div className="flex-column home_top_banner">
+                        <div className="htb_wrap">{/* <times :onlineEndTime="onlineEndTime" /> */}</div>
+                        <div className="flex-center htb_info">
+                            <div className="flex-column htb_info_item">
+                                <span>参评单位</span>
+                                <span>5</span>
+                            </div>
+                            <div className="flex-column htb_info_item">
+                                {/* <span>累积投票</span><span>{{ voteCount }}</span> */}
+                            </div>
+                            <div className="flex-column htb_info_item">
+                                {/* <span>访问次数</span><span>{{ voteViewCount }}</span> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 export default OnlineVote
